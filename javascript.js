e8fe32682b057d46849dead85598e2424cb76770
DarkBlueButterfly <208701196+DarkBlueButterfly@users.noreply.github.com>
2025-05-16 13:19:26 -0600

    Figure out how to make the first function work.
    Managed to get getComputerChoice to work many different ways but each
    way was not a function, until the last method.

--- javascript.js
+++ javascript.js
@@ -1,2 +1,44 @@
 //console.log("hello world again!");
+//randomizing the computer's choice:
+/*function getComputerChoice() {
+    return Math.floor(Math.random()*3);
+
+}*/
+/* //this isn't a function...
+let computerChoice = Math.floor(Math.random()*3);
+
+if (computerChoice === 0){
+    console.log("Rock"); 
+} else if(computerChoice === 1) {
+        console.log("Paper");
+} else if(computerChoice === 2) {
+    console.log("Scissors");
+}
+
+console.log(computerChoice); //check
+*/
+
+//used an array...
+/*function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    const randomNum = Math.floor(Math.random()*3);
+    return choices[randomNum];
+}
+
+console.log(getComputerChoice());
+*/
+
+function getComputerChoice() {
+    return Math.floor(Math.random()*3);
+}
+
+let computerChoice = getComputerChoice();
+
+if (computerChoice === 0){
+    console.log("Rock"); 
+} else if(computerChoice === 1) {
+    console.log("Paper");
+} else if(computerChoice === 2) {
+    console.log("Scissors");
+}
 
