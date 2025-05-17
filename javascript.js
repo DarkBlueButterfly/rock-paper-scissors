375801ec34dd6298c796d7e2543599e94a4e6398
DarkBlueButterfly <208701196+DarkBlueButterfly@users.noreply.github.com>
2025-05-17 09:50:11 -0600

    Update code for humanChoice
    Added code to reprompt human if they didn't pick rock, paper or
    scissors. Also removed some unused code.

--- javascript.js
+++ javascript.js
@@ -1,32 +1,5 @@
 //console.log("hello world again!");
 //randomizing the computer's choice:
-/*function getComputerChoice() {
-    return Math.floor(Math.random()*3);
-
-}*/
-/* //this isn't a function...
-let computerChoice = Math.floor(Math.random()*3);
-
-if (computerChoice === 0){
-    console.log("Rock"); 
-} else if(computerChoice === 1) {
-        console.log("Paper");
-} else if(computerChoice === 2) {
-    console.log("Scissors");
-}
-
-console.log(computerChoice); //check
-*/
-
-//used an array...
-/*function getComputerChoice() {
-    const choices = ["rock", "paper", "scissors"];
-    const randomNum = Math.floor(Math.random()*3);
-    return choices[randomNum];
-}
-
-console.log(getComputerChoice());
-*/
 
 function getComputerChoice() {
     return Math.floor(Math.random()*3);
@@ -35,34 +8,38 @@ function getComputerChoice() {
 let computerChoice = getComputerChoice();
 
 if (computerChoice === 0){
-    console.log("rock"); 
+    computerChoice = ("rock"); 
 } else if(computerChoice === 1) {
-    console.log("paper");
+    computerChoice = ("paper");
 } else if(computerChoice === 2) {
-    console.log("scissors");
+    computerChoice = ("scissors");
 }
 
-let getHumanChoice = prompt("Rock, paper or scissors?");
-let humanChoice = getHumanChoice.toLowerCase();
+console.log(computerChoice);
 
+let userChoice = prompt("Rock, paper or scissors?", "rock");
+let humanChoice = userChoice.toLowerCase();
+
+while (true) {
+    if (humanChoice == "rock") {
+        break;
+    } else if (humanChoice == "paper") {
+        break;
+    } else if (humanChoice == "scissors") {
+        break;
+    } else {
+        humanChoice = prompt("Please choose rock, paper or scissors");
+    }
+}        
+    
 console.log(humanChoice);
-
-
-
-/*
-function getHumanChoice() {
-    return prompt("Rock, paper or scissors?");
-}*/
-
-/*
-//console.log(getHumanChoice());
-let humanChoice = getHumanChoice();
-humanChoice.sort((a,b) => a.localeCompare(b, "en", {ignorePunctuation: true}));
-
 
 let humanScore = 0;
 let computerScore = 0;
 
+
+
+/*
 //humanChoice = humanChoice.toLowerCase() === humanChoice.toLowerCase();
 
 //console.log(getHumanChoice());
