46106349b8b79c0cdd91301df1c89ac024063070
DarkBlueButterfly <208701196+DarkBlueButterfly@users.noreply.github.com>
2025-05-19 09:45:28 -0600

    Code clean up
    Removed unused code and added some comments.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,3 @@
-//console.log("hello world again!");
 //randomizing the computer's choice:
 function getComputerChoice() {
     result = Math.floor(Math.random()*3);
@@ -14,6 +13,7 @@ function getComputerChoice() {
 let computerChoice = getComputerChoice();
 console.log(computerChoice); //check
 */
+
 //prompt user: 
 function getHumanChoice() {
     userChoice = prompt("Rock, paper or scissors?", "rock");
@@ -35,32 +35,8 @@ function getHumanChoice() {
 let humanChoice = getHumanChoice();
 console.log(humanChoice); //check
 */
-//let humanScore = 0;
-//let computerScore = 0;
 
-//to determine the winner:
-/*
-function playRound(computerChoice, humanChoice) {
-let humanScore = 0;
-let computerScore = 0;
-    if (computerChoice === humanChoice) {
-        console.log("It's a tie!");
-    } else if (
-        (humanChoice === "rock" && computerChoice === "scissors") ||
-        (humanChoice === "paper" && computerChoice === "rock") ||
-        (humanChoice === "scissors" && computerChoice === "paper")
-    ) {
-        humanScore++; 
-        console.log(`You win! ${humanChoice} beats ${computerChoice}!`);
-    } else {
-        computerScore++; 
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}...`);
-    }
-}*/
-
-//const humanSelection = getHumanChoice();
-//const computerSelection = getComputerChoice();
-
+//Play 5 rounds:
 function playGame() {
     let humanScore = 0;
     let computerScore = 0;
@@ -68,7 +44,7 @@ function playGame() {
     for (let i = 1; i < 6; i++) {
         const humanSelection = getHumanChoice();
         const computerSelection = getComputerChoice();
-
+        //to determine the winner:
         function playRound(computerChoice, humanChoice) {
             if (computerChoice === humanChoice) {
                 console.log(`It's a tie! You both chose ${humanChoice}!`);
@@ -98,42 +74,3 @@ function playGame() {
 }
 
 playGame();
-//const humanSelection = getHumanChoice();
-//const computerSelection = getComputerChoice();
-
-//playRound(humanSelection, computerSelection);
-
-//console.log(humanScore)
-
-
-/*
-//play 5 rounds:
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-
-    for (let i = 0; i < 5; i++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        const result = playRound(humanSelection, computerSelection);
-        
-        console.log(`Round ${i + 1}: You chose ${humanSelection}, computer chose ${computerSelection}.`);
-
-        if (result === `You win! ${humanSelection} beats ${computerSelection}!`) {
-            humanScore++;
-        } else if (result === `You lose! ${computerSelection} beats ${humanSelection}...`) {
-            computerScore++;
-        }
-    }
-
-    if (humanScore > computerScore) {
-        console.log("You won the series!");
-    } else if (computerScore > humanScore) {
-        console.log("You lost the game!");
-    } else {
-        console.log("It's a tie game!")
-    }
-    
-}*/
-
-//playGame();
